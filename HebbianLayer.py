06747278622470ecbd2249b7340caec508ca14b5
frellan <fredrik.kindstrom@gmail.com>
2021-02-12 15:44:11 +0100

    What is going on

--- HebbianLayer.py
+++ HebbianLayer.py
@@ -7,31 +7,47 @@ class HebbianLayer(nn.Module):
         super(HebbianLayer, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = torch.Tensor(out_features, in_features)
-        self.bias = torch.Tensor(out_features)
+        self.weight = torch.rand(out_features, in_features)
+        self.bias = torch.zeros(out_features)
 
         self.learning_rate = learning_rate
         self.learning = True
 
-    def activation(self, input):
-        return F.linear(input, self.weight, self.bias)
+    def output(self, input):
+        print("X=")
+        print(input)
+        print("weight=")
+        print(self.weight)
+        print("bias=")
+        print(self.bias)
+        # return F.linear(input, self.weight, self.bias)
+        output = input.matmul(self.weight.t())
+        output += self.bias
+        print("x * w^T + b =")
+        print(output)
+        return output
 
     def forward(self, input):
-        y = self.activation(input)
+        y = self.output(input)
         if self.learning:
             self.update_weight(input)
         return y
 
     def update_weight(self, input):
-        with torch.no_grad():
-            for x_input in input:
-                x = x_input.reshape(1, len(x_input))
-                y = self.activation(x_input)
-                y_length = len(y)
-                y = y.reshape(1, y_length)
-                y_t = y.reshape(y_length, 1)
-                d_w = self.learning_rate * ((x * y_t) - torch.tril(y * y_t, diagonal=-1) @ self.weight)
-                self.weight += d_w
+        count = 0
+        for x_input in input:
+            if count >= 1:
+                continue
+            x = x_input.reshape(1, len(x_input))
+            y = self.output(x_input)
+            y_length = len(y)
+            y = y.reshape(1, y_length)
+            y_t = y.reshape(y_length, 1)
+
+
+            d_w = self.learning_rate * ((x * y_t) - (torch.tril(y * y_t, diagonal=-1) @ self.weight))
+            self.weight += d_w
+            count += 1
 
     def extra_repr(self):
         return 'in_features={}, out_features={}, bias={}'.format(
